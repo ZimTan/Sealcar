45529633c61a66113876ca47d101e72d798565ea
corentin <corentin.bunel@epita.fr>
2021-09-14 16:24:56 +0200

    [ADD][autocar_config.py] add interface file

---
+++ src/autocar_config.py
@@ -0,0 +1,31 @@
+# Configuration file for the autocar controller:
+
+# controller config:
+controller_config = {
+    'dead_zone': 0.15,
+    'fix_steer': 0.0,
+    'path': 'models_trainned/nvidia_speed_test'
+}
+
+# viewer config:
+viewer_config = {
+    'screen_size': (640, 480)
+}
+
+# recorder config:
+recorder_config = {
+    'path': '../dataset/rrl1/'
+}
+
+# car config:
+car_config = {
+    # 'host': 'sim.diyrobocars.fr',
+    'host': 'localhost',
+    #'host': '192.168.103.69',
+    'port': 9091,
+
+    'car_name': None,
+    'font_size': None,
+    'body_style': None,
+    'body_rgb': None
+}
