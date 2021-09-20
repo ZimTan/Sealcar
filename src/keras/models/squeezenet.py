b3ba7a23055820b7618d01af817862092d1ec9a0
Apolline Wasik <apolline.wasik@epita.fr>
2021-09-20 16:45:27 +0000

    [FIX][SQUEEZENET] fix initialization of squeezenet neural network, avoiding conflict between layers
    Signed-off-by: Apolline Wasik <apolline.wasik@epita.fr>

--- src/keras/models/squeezenet.py
+++ src/keras/models/squeezenet.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import MaxPooling2D, Dropout, Conv2D, GlobalAveragePooling2D, Dropout, Dense, Concatenate
+from tensorflow.keras.layers import MaxPooling2D, Dropout, Conv2D, GlobalAveragePooling2D, Dropout, Dense,Concatenate
 
 
 
@@ -8,27 +8,43 @@ class SqueezenetModel(tf.keras.Model):
     def __init__(self, unput_shape, num_outputs):
         super(SqueezenetModel, self).__init__()
 
-        self.conv1 = Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), padding='same', activation='relu',
+        self.conv1 = Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), padding='valid', activation='relu',
                 kernel_initializer=tf.keras.initializers.he_uniform())
 
         self.conv2 = Conv2D(filters=32, kernel_size=(1, 1), activation='relu',
           strides=(1, 1), kernel_initializer=tf.keras.initializers.he_uniform())
 
-        self.conv3 = Conv2D(filters=16, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
-        self.conv4 = Conv2D(filters=64, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv3 = Conv2D(filters=16, kernel_size=(1, 1), activation='relu', padding='valid', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv4 = Conv2D(filters=64, kernel_size=(1, 1), activation='relu', padding='valid', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv5 = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+
+        self.conv3_bis = Conv2D(filters=16, kernel_size=(1, 1), activation='relu', padding='valid', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv4_bis = Conv2D(filters=64, kernel_size=(1, 1), activation='relu', padding='valid', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv5_bis = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
 
         self.conv6 = Conv2D(filters=32, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv7 = Conv2D(filters=128, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv8 = Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
 
+        self.conv6_bis = Conv2D(filters=32, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv7_bis = Conv2D(filters=128, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv8_bis = Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+
         self.conv9 = Conv2D(filters=48, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv10 = Conv2D(filters=192, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv11 = Conv2D(filters=192, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
 
+        self.conv9_bis = Conv2D(filters=48, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv10_bis = Conv2D(filters=192, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv11_bis = Conv2D(filters=192, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+
         self.conv12 = Conv2D(filters=64, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv13 = Conv2D(filters=256, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
         self.conv14 = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+
+        self.conv12_bis = Conv2D(filters=64, kernel_size=(1, 1), activation='relu', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv13_bis = Conv2D(filters=256, kernel_size=(1, 1), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
+        self.conv14_bis = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_uniform())
 
         self.pool1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')
 
@@ -50,9 +66,9 @@ class SqueezenetModel(tf.keras.Model):
         right = self.conv5(fire)
         fire = Concatenate(axis=-1)([left, right])
 
-        fire = self.conv3(fire)
-        left = self.conv4(fire)
-        right = self.conv5(fire)
+        fire = self.conv3_bis(fire)
+        left = self.conv4_bis(fire)
+        right = self.conv5_bis(fire)
         fire = Concatenate(axis=-1)([left, right])
 
         fire = self.pool1(fire)
@@ -62,9 +78,9 @@ class SqueezenetModel(tf.keras.Model):
         right = self.conv8(fire)
         fire = Concatenate(axis=-1)([left, right])
 
-        fire = self.conv6(fire)
-        left = self.conv7(fire)
-        right = self.conv8(fire)
+        fire = self.conv6_bis(fire)
+        left = self.conv7_bis(fire)
+        right = self.conv8_bis(fire)
         fire = Concatenate(axis=-1)([left, right])
 
         fire = self.pool1(fire)
@@ -74,9 +90,9 @@ class SqueezenetModel(tf.keras.Model):
         right = self.conv11(fire)
         fire = Concatenate(axis=-1)([left, right])
 
-        fire = self.conv9(fire)
-        left = self.conv10(fire)
-        right = self.conv11(fire)
+        fire = self.conv9_bis(fire)
+        left = self.conv10_bis(fire)
+        right = self.conv11_bis(fire)
         fire = Concatenate(axis=-1)([left, right])
 
         fire = self.conv12(fire)
@@ -84,9 +100,9 @@ class SqueezenetModel(tf.keras.Model):
         right = self.conv14(fire)
         fire = Concatenate(axis=-1)([left, right])
 
-        fire = self.conv12(fire)
-        left = self.conv13(fire)
-        right = self.conv14(fire)
+        fire = self.conv12_bis(fire)
+        left = self.conv13_bis(fire)
+        right = self.conv14_bis(fire)
         fire = Concatenate(axis=-1)([left, right])
 
         dropout = self.dropout1(fire, training)
