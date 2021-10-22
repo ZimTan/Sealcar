4e3d174f255804cfa91590e7cefc87a1e6f093aa
Tanzim Ahmed Mostafa <tanzim-ahmed.mostafa@epita.fr>
2021-10-22 19:09:59 +0200

    [UPDATE] fragmentation
    Signed-off-by: Tanzim Ahmed Mostafa <tanzim-ahmed.mostafa@epita.fr>

--- src/fragment.py
+++ src/fragment.py
@@ -4,10 +4,17 @@ import cv2
 import glob
 import os
 
+<<<<<<< HEAD
 dataset_path = "/home/corentin/SEAL/donkeyCar/Sealcar/dataset_real/21-16-51-573448/"
 frag_dataset_path = "/home/corentin/SEAL/donkeyCar/Sealcar/dataset_real/21-16-51-573448/frag/"
+=======
+dataset_path = "/home/tanz/Bureau/data_sealcar/"
+frag_dataset_path = "/home/tanz/Bureau/data_sealcar/frag/"
+>>>>>>> 94b05d3 ([UPDATE] fragmentation)
 
 imgs = []
+LEFT = 0
+RIGHT = 1
 #for img in os.listdir(dataset_path):
 #    imgs.append(dataset_path + img)
 
@@ -19,8 +26,19 @@ if (len(imgs) == 0):
 print(f'There are {len(imgs)} images in dataset.')
 
 THRESHOLD = 200
-HORIZON = 50
+HORIZON = 180
 #print(imgs[0])
+
+def update_threshold(image, i, j, cols):
+    result = 0
+    j += 1
+    while (j < cols - 1):
+        if (image[i,j] > result):
+            result = image[i,j]
+        j += 1
+
+    return result - 10;
+
 for img in imgs:
 
     print(img)
@@ -44,7 +62,7 @@ for img in imgs:
     img2 = np.load(img)
    # img2 = cv2.imread(img)
 
-    img_grey = 0.299 * img2[:,:,0] +  0.587 * img2[:,:,2] +  0.114 * img2[:,:,2] 
+    img_grey = 0.299 * img2[:,:,0] +  0.587 * img2[:,:,2] +  0.114 * img2[:,:,2]
    # plt.imshow(img_grey, cmap='gray', vmin=0, vmax=255)
     #plt.show()
 
@@ -59,26 +77,37 @@ for img in imgs:
             img_grey[i,:] = 0
     """
 
-    for i in range(rows - 1, HORIZON - 1, -1):
+    for i in range(HORIZON - 1):
+        for j in range(cols):
+            img_grey[i,j] = 0
+
+    for i in range(350, rows - 1):
+        for j in range(cols):
+            img_grey[i,j] = 0
+
+    for i in range(HORIZON, 350):
 
         found = False
+        brightness_sum = 0
 
         j = cols // 2
-        while j < cols - 1:
 
-            if (found == False and img_grey[i,j] > THRESHOLD) : #white
+        THRESHOLD = update_threshold(img_grey, i, j, cols)
 
+        while j < cols:
+
+            if (found == False and img_grey[i,j] > THRESHOLD): #white
                 found = True
-
                 if j < cols - 1:
                     j += 1
 
-                while (img_grey[i,j] > THRESHOLD - 30) :
+                while (j < cols and img_grey[i,j] > THRESHOLD - 5):
+                    brightness_sum += img_grey[i,j]
+                    j += 1
+                if (brightness_sum > THRESHOLD * 60):
+                    for k in range(cols // 2, cols - 1):
+                        img_grey[i,k] = 0
 
-                    if j < cols - 1 :
-                        j += 1
-                    else:
-                        break
 
             else:
                 img_grey[i,j] = 0
@@ -86,37 +115,39 @@ for img in imgs:
             j += 1
 
         found = False
+        brightness_sum = 0
         j = cols // 2
+
+        THRESHOLD = update_threshold(img_grey, i, 0, j)
+
         while j > 0:
-
             if (found == False and img_grey[i,j] > THRESHOLD) : #white
-
                 found = True
+                if j > 0 :
+                    j -= 1
 
-                if j == 0 :
-                    continue
-
-                j -= 1
-
-
-                while (img_grey[i,j] > THRESHOLD - 30) :
-
-                    if j > 0 :
-                        j -= 1
-                    else:
-                        break
+                while (j > 0 and img_grey[i,j] > THRESHOLD - 5):
+                    brightness_sum += img_grey[i,j]
+                    j -= 1
+                if (brightness_sum > THRESHOLD * 60):
+                    for k in range(cols // 2):
+                        img_grey[i,k] = 0
 
             else:
                 img_grey[i,j] = 0
             j -= 1
 
-    ''' 
-    fig, axs = plt.subplots(ncols=2)
-    axs[0].imshow(final_img)
-    axs[1].imshow(img_grey)
-    plt.show()
-    '''
+
+
+
+
+
 
     final_img = cv2.merge((img_grey, img_grey, img_grey))
     #print(final_img.flatten() > 0)
     np.save(frag_dataset_path + img.split('/')[-1], final_img)
+
+    fig, axs = plt.subplots(ncols=2)
+    axs[0].imshow(final_img)
+    axs[1].imshow(np_img)
+    plt.show()
