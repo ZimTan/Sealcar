9f1d3aa90ad168c163d7a858b404efbe6aad7f39
cloud441 <kevin.guillet@epita.fr>
2021-11-09 13:11:39 +0100

    [MODEL]: train models on competition dataset

--- src/pytorch/config.py
+++ src/pytorch/config.py
@@ -3,16 +3,18 @@
 #IMAGE_DIM = (120, 160, 1)
 IMAGE_DIM = (480, 640, 1)
 
-DATASETS_SEG_PATH = ["dataset_real/23-14-47-853175/frag"] 
+DATASETS_SEG_PATH = ["datasets/23-14-47-853175/frag", "datasets/23-14-52-400121/frag",
+                    "datasets/23-14-57-227476/frag", "datasets/23-15-05-852589/frag"]
 
-DATASETS_PATH = ["dataset_real/23-14-47-853175"]
+DATASETS_PATH = ["datasets/23-14-47-853175", "datasets/23-14-52-400121",
+                    "datasets/23-14-57-227476", "datasets/23-15-05-852589"]
 
 
-MODEL_SAVE_PATH = "src/pytorch/models/segnvidia"
-MODEL_SAVE_PATH_SEG = "src/pytorch/models/cnnseg"
+MODEL_SAVE_PATH = "models/segnvidia_1"
+MODEL_SAVE_PATH_SEG = "models/cnnseg_1"
 
-TRAIN_SIZE = 0.7
-VALID_SIZE = 0.2
+TRAIN_SIZE = 0.8
+VALID_SIZE = 0.1
 TEST_SIZE = 0.1
 
 MODEL_NAME = "nvidia-speed"
